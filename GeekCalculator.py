156d7623e953ac5dc2609c61bcf98f5ba1982b26
Litrin Jiang <litrin.jiang@intel.com>
2015-02-12 09:01:31 +0800

    finished ver 1

--- GeekCalculator.py
+++ GeekCalculator.py
@@ -2,27 +2,23 @@ import sublime_plugin
 import time
 
 class GeekCalculatorCommand(sublime_plugin.TextCommand):
-	
-	Separator = "\n" 
+    Separator = "\n" 
 
-	def run(self, edit):
-		selected_regions = self.view.sel()
-		for region in selected_regions:
-			selected = self.view.substr( region );
-      if re.match('^[0-9+\-*/%\(\). ]+$', expr):
-        result = eval( expr )
-			self.view.replace( edit, region, str( result ) )
-
+    def run(self, edit):
+        selected_regions = self.view.sel()
+        for region in selected_regions:
+            selected = self.view.substr( region )
+            self.view.replace( edit, region, self.calculate(selected) )
     def calculate(self, selected):
-    	selected = selected.split(self.Separator)
-    	result = list()
-    	for expr in selected:
-    		try:
-    			formula = "%s=%s" % (expr, eval(expr))
-    		except:
-    			formula = expr
-    		result.append(formula)
+        selected = selected.split(self.Separator)
+        result = list()
+        for expr in selected:
+            try:
+                formula = "%s=%s" % (expr, eval(expr))
+            except:
+                formula = expr
+            result.append(formula)
 
-    	return self.Separator.join(result)
+        return self.Separator.join(result)
 
 
